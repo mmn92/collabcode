3931e582ad0748d626bdd6173de5a247cc32ba2d
mmn92 <matheus.mota.nogueira@gmail.com>
2019-08-24 23:17:16 -0300

    [014] - Desafios Básico, Intermediário e Avançado

--- loja-fone/js/product-description.js
+++ loja-fone/js/product-description.js
@@ -1,7 +1,28 @@
-const $heart = window.document.querySelector(".hearts");
+const $heart = document.querySelector(".hearts");
+const $star = document.querySelectorAll(".star");
 
 $heart.addEventListener("click", handleClick);
 
+$star.forEach(function(star) {
+  star.addEventListener("click", handleClickStar);
+});
+
 function handleClick() {
-  console.log("clicou");
+  $heart.classList.toggle("-active");
+}
+
+function handleClickStar() {
+  let current = this;
+  current.classList.toggle("-full");
+  if (current.classList.contains("-full")) {
+    while (current.previousElementSibling) {
+      current = current.previousElementSibling;
+      current.classList.add("-full");
+    }
+  } else {
+    while (current.nextElementSibling) {
+      current.nextElementSibling.classList.remove("-full");
+      current = current.nextElementSibling;
+    }
+  }
 }
