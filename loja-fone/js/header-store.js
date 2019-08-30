0971fca84f7de522848a4270d84fd0c26f187060
mmn92 <matheus.mota.nogueira@gmail.com>
2019-08-29 23:52:38 -0300

    [018] - Desafio a esquerda

--- loja-fone/js/header-store.js
+++ loja-fone/js/header-store.js
@@ -1,7 +1,14 @@
 const $searchIcons = document.querySelectorAll(".search-icon");
+const $menuIcons = document.querySelectorAll(".menu-icon");
 
 $searchIcons.forEach($searchIcon => {
   $searchIcon.addEventListener("click", () => {
     document.querySelector(".search-wrapper").classList.toggle("-active");
+  });
+});
+
+$menuIcons.forEach($menuIcon => {
+  $menuIcon.addEventListener("click", () => {
+    document.querySelector(".menu-wrapper").classList.toggle("-active");
   });
 });
