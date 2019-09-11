80546af5057bfd5c1734b08832bde1e3007bf8ba
mmn92 <matheus.mota.nogueira@gmail.com>
2019-09-11 00:05:40 -0300

    reorganiza codigo - aula 024

--- jogo-da-memoria/src/pages/game/index.js
+++ jogo-da-memoria/src/pages/game/index.js
@@ -1,10 +1,18 @@
-createMemoryCardFront();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
-createMemoryCard();
+const $root = document.querySelector("#root");
+
+const $cardsWrapper = createCardsWrapper();
+const $memoryCardFlipped = createMemoryCard("-flipped", "c");
+const $memoryCard = createMemoryCard();
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFlipped);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+
+$root.insertAdjacentElement("beforeend", $cardsWrapper);
