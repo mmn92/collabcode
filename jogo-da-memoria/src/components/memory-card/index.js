b26e4c09a58f9157d8f7777c10d1cb89231a8b16
mmn92 <matheus.mota.nogueira@gmail.com>
2019-09-11 00:06:04 -0300

    [024] - Desafios Básico e Intermediário

--- jogo-da-memoria/src/components/memory-card/index.js
+++ jogo-da-memoria/src/components/memory-card/index.js
@@ -1,31 +1,14 @@
-function createMemoryCard() {
-  const $memoryCard = document.createElement("article");
+const createMemoryCard = (modifier = "", icon = "collabcode") => `
+    <article class="memory-card ${modifier}">
+      <img 
+        class='icon' 
+        src='assets/icon-${icon}.png' 
+        alt='Gueio mascote da CollabCode' 
+        onClick="handleClick()"
+      />
+    </article>
+  `;
 
-  const $iconCollab = `
-        <img 
-            class='icon' 
-            src='assets/icon-collabcode.png' 
-            alt='Gueio mascote da CollabCode' 
-        />
-    `;
-
-  $memoryCard.classList.add("memory-card");
-  $wrapCards.insertBefore($memoryCard, null);
-  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
-}
-
-function createMemoryCardFront() {
-  const $flippedCard = document.createElement("article");
-
-  const $iconCpp = `
-    <img 
-        class="icon" 
-        src="assets/icon-c.png" 
-        alt="icone c++" 
-    />
-`;
-
-  $flippedCard.classList.add("memory-card", "-flipped");
-  $wrapCards.insertBefore($flippedCard, null);
-  $flippedCard.insertAdjacentHTML("afterbegin", $iconCpp);
+function handleClick() {
+  console.log("asdasda");
 }
